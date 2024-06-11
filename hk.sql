27b942badc4ede97967d09886b40f52617aee3fb
harikrushna0816 <parmarhari983@gmail.com>
2024-06-11 10:13:45 +0530

    Add files via upload

---
+++ hk.sql
@@ -0,0 +1,18 @@
+use hari
+create table hk1(std int,
+				 sname varchar(90),
+				 studentid int,
+				 per float)
+
+insert into hk1(std ,sname,studentid,per )
+values(12 ,'Hari',123,89.9)
+
+insert into hk1 values(3,'Niravi',101,99),
+						(4,'Niravp',102,99),
+						(5,'Niravo',103,99),
+						(6,'N',104,99),
+						(7,'Ni',105,99),
+						(8,'Nir',106,99),
+						(9,'Nira',107,99)
+
+select * from hk1
